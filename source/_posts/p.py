359254fb7722383de2540951cd3e4f343dd8190a
Cishenn <740804059@qq.com>
2021-04-21 18:43:14 +0800

    feat: update several articles

--- source/_posts/p.py
+++ source/_posts/p.py
@@ -6,9 +6,8 @@ for file in os.listdir('.'):
             content = f.read()    
         import re
         print(content)
-        if (content.startswith('\n')):
-            content = content[1:]
-        print(content)
+        content = re.sub(r'^<!--.*?-->', '', content, flags=re.DOTALL)
+        content = content.lstrip()
         with open(file, 'w', encoding='utf-8') as f: 
             f.write(content)
         # input()
